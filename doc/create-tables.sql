f553c98b37635d69d08272b4ecd048eab0d81d62
iquesadmor <iquesadmor@educacion.navarra.es>
2026-02-27 13:59:36 +0100

    Commit de los inserts de la base de datos

--- doc/create-tables.sql
+++ doc/create-tables.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS k1furydb.usuario (
 	id int AUTO_INCREMENT,
     nombre varchar(30) DEFAULT NULL,
     apellido varchar(30) DEFAULT NULL,
+    contraseña varchar(30) DEFAULT NULL,
     rol int,
     peso int,
 
